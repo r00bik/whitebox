9d78734798d202c2fcf13a459f1f63c3ff65a64d
vlad balakin <vladbalakin@gmail.com>
2025-07-14 15:40:39 +0500

    refactor(web): реорганизация импортов в index.ts для улучшения читаемости
    - Перемещён импорт глобальных стилей выше импортов библиотек и компонентов
    - Упорядочены импорты для единообразия и лучшей структуры файла

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescri
 import { globalIgnores } from "eslint/config";
 import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -12,7 +13,12 @@ export default defineConfigWithVueTs(
   {
     name: "app/files-to-lint",
     files: ["**/*.{ts,mts,tsx,vue}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
